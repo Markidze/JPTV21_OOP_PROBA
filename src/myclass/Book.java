ef2cb597e126eba3ad681ab160f50c3249dcd68f
pupil <pupil@ivkhk.ee>
2022-10-05 14:26:54 +0300

    OOP

---
+++ src/myclass/Book.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclass;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author pupil
+ */
+public class Book {
+    private Author[] authors;
+    private String caption;
+    
+
+    public Book() {
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+    
+    public void setAuthors(Author[] authors) {
+        this.authors = authors;
+    }
+    public void addAuthor(Author author) {
+        int countAuthors = this.authors.length;
+        Author[] newAuthors = Arrays.copyOf(this.authors, countAuthors+1);
+        newAuthors[countAuthors] = author;
+        this.authors = newAuthors;
+    }
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "book{" + "authors=" + Arrays.toString(authors) + ", caption=" + caption + '}';
+    }
+
+    
+}
